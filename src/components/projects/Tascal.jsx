97406c1383a74eabf42733bc7ac6431bfea2b31e
matthias <mr3lucciola@gmail.com>
2019-12-17 04:13:27 -0500

    added basic tascal component; updated diffuse site and image

--- src/components/projects/Tascal.jsx
+++ src/components/projects/Tascal.jsx
@@ -1,4 +1,4 @@
-// App.jsx > Body.jsx > Main.jsx > Diffuse.jsx
+// App.jsx > Body.jsx > Main.jsx > Tascal.jsx
 
 // react
 import React from 'react';
@@ -8,24 +8,22 @@ import ProjectIntro       from '../display/ProjectIntro';
 import ProjectPreview     from '../display/ProjectPreview';
 // import ProjectDescription from '../display/ProjectDescription';
 
-export default function Diffuse(){
+export default function Tascal(){
     const projectObj = {
-        title: "Diffuse",
-        repoURL: "https://github.com/mattrlucciola/diffuse.git",
-        siteURL: "https://diffuse.dev",
+        title: "Tascal",
+        repoURL: "https://github.com/mattrlucciola/Tascal.git",
+        siteURL: "https://tascal.dev",
         description: `\
-            Collaborative songwriting software.\
-            Register, sign in, create and save projects to your profile.\
-            Add others to contribute to your songs and track the history of the songs progression!\
+            Project management, note taking app.
         `,
         stackObj: {
             frontend: ['React.js'],
             backend: ['MongoDB, Node.js, Django'],
         },
-        imgURL: "/assets/images/diffuse-preview.png",
+        imgURL: "/assets/images/Tascal-preview.png",
     }
     return(
-        <div className="Diffuse project">
+        <div className="Tascal project">
             <div className="intro-continer right"><ProjectIntro projectObj={projectObj} /></div>
             <div className="pd-container flexcol left">
                 <ProjectPreview imgURL={projectObj['imgURL']} siteURL={projectObj['siteURL']} />
