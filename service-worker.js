2f42b1e3339aae7c50c8ad0795a73346f5a44e2d
matthias <mr3lucciola@gmail.com>
2019-12-19 16:11:12 -0500

    (re)building and deploying

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.72fb1846c159f809740872bde7218994.js"
+  "/precache-manifest.46c121ff4c8a70926b6b854be5329710.js"
 );
 
 self.addEventListener('message', (event) => {
