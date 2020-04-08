83a2014a472b405dad8a0a3d270da59c2c5efa83
matthias <mr3lucciola@gmail.com>
2020-04-08 19:20:50 -0400

    changing spacing for nav css and adding proj obj to nav

--- src/components/main/nav/Nav.jsx
+++ src/components/main/nav/Nav.jsx
@@ -1,11 +1,20 @@
 // App.jsx > Main.jsx > Nav.jsx
 // react
 import React from 'react';
+// redux
+import {useSelector} from 'react-redux';
 // style
 import './Nav.css';
+// global vars
+import {projectObjectsArr} from '../body/projectObjectsArr';
 
 // main
 export default function Nav(){
+    console.log(projectObjectsArr)
+    // redux state
+    let {currentProject} = useSelector(state => state['project'])
+    console.log(currentProject)
+    // build fxns
     return(
         <div className="Nav">
             <div className="contact">Contact</div>
