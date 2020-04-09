639137bfcf3ad49c7ae5b49593e754b574bb7965
matthias <mr3lucciola@gmail.com>
2020-04-09 00:37:44 -0400

    images kind of set up

--- src/components/main/body/project/projectPreview/ProjectPreview.jsx
+++ src/components/main/body/project/projectPreview/ProjectPreview.jsx
@@ -6,15 +6,18 @@ import './ProjectPreview.css';
 
 // main
 export default function ProjectPreview({imgURL, siteURL}){
-    const buildImgElem = () => {
-
+    const buildImgElem = (imgURL, siteURL) => {
         return (
-            <a target="_blank" rel="noopener noreferrer" href={siteURL}><img src={imgURL} alt="" className="preview-img"/></a>
+            <div className="imgContainer" >
+                {/* <a target="imgLink" rel="noopener noreferrer" href={siteURL}> */}
+                <img src={imgURL} alt="" className="preview-img"/>
+                {/* </a> */}
+            </div>
         )
     }
     return(
         <div className="ProjectPreview">
-            {imgURL && buildImgElem()}
+            {imgURL && buildImgElem(imgURL, siteURL)}
         </div>
     )
 }
