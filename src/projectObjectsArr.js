881829c1e48b7abf7fdd51360513cb70691f1081
matthias <mrlucciola@protonmail.com>
2021-10-28 16:25:09 -0400

    updated the name in package.json; removed assets from top level outside of src

--- src/projectObjectsArr.js
+++ src/projectObjectsArr.js
@@ -4,7 +4,7 @@ export const projectObjectsArr = [
   // },
   // {
   //     title: "Tascal",
-  //     repoURL: "https://github.com/0xseagull/Tascal.git",
+  //     repoURL: "https://github.com/mrlucciola/Tascal.git",
   //     siteURL: "https://tascal.dev",
   //     description: `\
   //         Project management, scheduling, productivity-tracking, and note taking app.
@@ -26,48 +26,46 @@ export const projectObjectsArr = [
       frontend: ["React.js", "D3.js", "Redux.js"],
       backend: ["MongoDB", "Node.js", "Express.js"],
     },
-    // imgURL: require("./assets/images/envest-preview.png"),
     imgURL: require("./images/envest-preview.png"),
-    // /Users/matthias/projects/dev-site/assets/images/envest-preview.png
   },
   {
     title: "Diffuse",
-    repoURL: "https://github.com/0xseagull/diffuse.git",
+    repoURL: "https://github.com/mrlucciola/diffuse.git",
     siteURL: "https://diffuse.dev",
     description: `\
-            Collaborative songwriting software.\
-            Register, sign in, create and save projects to your profile.\
-            Add others to contribute to your songs and track the history of the songs progression!\
-        `,
+      Collaborative songwriting software.\
+      Register, sign in, create and save projects to your profile.\
+      Add others to contribute to your songs and track the history of the songs progression!\
+    `,
     stackObj: {
       frontend: ["React.js", "D3.js", "Redux.js"],
       backend: ["MongoDB", "Node.js", "Django", "Django Rest Framework"],
     },
-    imgURL: "/assets/images/diffuse-preview.png",
+    imgURL: require("./images/diffuse-preview.png"),
   },
   {
     title: "BDA",
-    repoURL: "https://github.com/0xseagull/bpm-detection-algorithm.git",
-    siteURL: "https://0xseagull.github.io/bpm-detection-algorithm/",
+    repoURL: "https://github.com/mrlucciola/bpm-detection-algorithm.git",
+    siteURL: "https://mrlucciola.github.io/bpm-detection-algorithm/",
     description: `
-            Detects BPM and displays track information from user-added songs using a simple peak-detection algorithm
-        `,
+      Detects BPM and displays track information from user-added songs using a simple peak-detection algorithm
+    `,
     stackObj: {
       frontend: ["JavaScript (ES6)", "Web Audio API", "React", "Redux"],
     },
-    imgURL: "/assets/images/bda-preview.png",
+    imgURL: require("./images/bda-preview.png"),
   },
   {
     title: "Cofi",
-    repoURL: "https://github.com/0xseagull/cofi.git",
-    siteURL: "https://0xseagull.github.io/cofi/",
+    repoURL: "https://github.com/mrlucciola/cofi.git",
+    siteURL: "https://mrlucciola.github.io/cofi/",
     description: `\
-            A digital interface for synthesizing musical sequences in a step-wise manner.\
-            Simply point and click on the note you want to activate, and the time at which to activate.\
-        `,
+      A digital interface for synthesizing musical sequences in a step-wise manner.\
+      Simply point and click on the note you want to activate, and the time at which to activate.\
+    `,
     stackObj: {
       frontend: ["React.js", "Web Audio API", "Redux.js"],
     },
-    imgURL: "/assets/images/cofi-preview.png",
+    imgURL: require("./images/cofi-preview.png"),
   },
 ];
