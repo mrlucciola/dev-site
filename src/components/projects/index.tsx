95a2966a7102689b2b98cb0860b8f60ad73077ea
mrlucciola <mrlucciola@pm.me>
2022-08-22 17:40:48 -0400

    cards completed; nav fixed; breakpoints added

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,12 +1,12 @@
 // react
 import { FC } from "react";
 // style
-import { Grid } from "@mui/material";
+import { List } from "@mui/material";
 // state
+import { observer } from "mobx-react-lite";
 import { useAppContext } from "../../mobx/context";
 // components
 import ProjectCard from "./ProjectCard";
-import { observer } from "mobx-react-lite";
 
 /**
  * main
@@ -16,20 +16,11 @@ const ProjectList: FC = () => {
   const projectArrLen: number = useAppContext((s) => s.main.projects).length;
 
   // build the array of elements
-  // Array.from(Array(projectLen).keys())
   const projectElems = Array.from(Array(projectArrLen).keys()).map((idx) => {
     return <ProjectCard projectIdx={idx} key={idx} />;
   });
 
-  return (
-    <Grid
-      item
-      container
-      // className="ProjectList"
-    >
-      {projectElems}
-    </Grid>
-  );
+  return <List>{projectElems}</List>;
 };
 
 export default observer(ProjectList);
