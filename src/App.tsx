60d0de2679be9dc89d5f30059e5cce4994fad88c
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-10-03 22:47:07 -0400

    opt: optimize mui imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 // mui
-import { Grid } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
 // state
 import { observer } from "mobx-react-lite";
 // components
@@ -12,9 +12,9 @@ import Nav from "./components/Nav";
 const App: FC = () => {
   return (
     <Grid
+      className="App"
       container
       direction="column"
-      className="App"
       sx={{
         height: "100vh",
         minHeight: "100vh",
