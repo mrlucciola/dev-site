974ba2b1cf0948d03e468b607dd3c9d1ee27bf80
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-10-03 23:56:25 -0400

    update projects

--- src/mobx/data/projectsArr.tsx
+++ src/mobx/data/projectsArr.tsx
@@ -54,21 +54,21 @@ export const toolsIconMap: { [key: string]: JSX.Element } = {
 };
 
 export const projectsArr = [
-  new Project(
-    "CDP with Yield-Farming Solana App",
-    `Built and deployed a Solana-native collateralized-debt-position platform for yield-bearing assets on mainnet.
-    Issuing debt (minting stablecoin) while earning yield (on 'yield farms', liquidity mining, etc)
-    on what otherwise would-be idle assets - all while maintaining full self-custody of utilized assets.
-    \n\nLead an Agile team of 10 developers, lead the design and technical implementation of the product
-    line and was a major contributor on product decisions. Implemented Agile methodology and standards for collaborative
-    development. Lead code reviews and managed external code audits.`,
-    new Map([
-      ["Blockchain", ["Solana", "Anchor.rs", "Rust"]],
-      ["Backend", ["Typescript", "Node.js", "PostgreSQL", "Express.js"]],
-      ["Frontend", ["Typescript", "React.js", "Redux.js"]],
-    ]),
-    "https://github.com/mrlucciola/cdp"
-  ),
+  // new Project(
+  //   "CDP with Yield-Farming Solana App",
+  //   `Built and deployed a Solana-native collateralized-debt-position platform for yield-bearing assets on mainnet.
+  //   Issuing debt (minting stablecoin) while earning yield (on 'yield farms', liquidity mining, etc)
+  //   on what otherwise would-be idle assets - all while maintaining full self-custody of utilized assets.
+  //   \n\nLead an Agile team of 10 developers, lead the design and technical implementation of the product
+  //   line and was a major contributor on product decisions. Implemented Agile methodology and standards for collaborative
+  //   development. Lead code reviews and managed external code audits.`,
+  //   new Map([
+  //     ["Blockchain", ["Solana", "Anchor.rs", "Rust"]],
+  //     ["Backend", ["Typescript", "Node.js", "PostgreSQL", "Express.js"]],
+  //     ["Frontend", ["Typescript", "React.js", "Redux.js"]],
+  //   ]),
+  //   "https://github.com/mrlucciola/cdp"
+  // ),
   new Project(
     "Aave Liquidation Engine",
     `\
@@ -86,65 +86,65 @@ export const projectsArr = [
     ]),
     "https://github.com/mrlucciola/aave-liquidation-engine"
   ),
-  new Project(
-    "Portfolio Page",
-    `\
-      Rocco Lucciola's portfolio page
-    `,
-    new Map([["Frontend", ["React.js", "MobX", "Typescript"]]]),
-    "https://github.com/mrlucciola/dev-site/",
-    undefined,
-    "https://mrlucciola.com/"
-  ),
-  new Project(
-    "PitchClean",
-    `\
-      Social media investment platform focused on renewables (mock backend)
-    `,
-    new Map([
-      ["Frontend", ["React.js", "D3.js", "Redux.js"]],
-      ["Backend", ["MongoDB", "Node.js", "Express.js"]],
-    ]),
-    "https://github.com/pitch-clean/client-ui",
-    "/images/envest-preview.png"
-    // envest.pro
-  ),
-  new Project(
-    "Diffuse",
-    `\
-      Collaborative songwriting software.\
-      Register, sign in, create and save projects to your profile.\
-      Add others to contribute to your songs and track the history of the songs progression!\
-    `,
-    new Map([
-      ["Frontend", ["React.js", "D3.js", "Redux.js"]],
-      ["Backend", ["MongoDB", "Node.js", "Django"]],
-    ]),
-    "https://github.com/mrlucciola/diffuse.git",
-    "/images/diffuse-preview.png"
-    // https://diffuse.dev
-  ),
-  new Project(
-    "BDA",
-    `
-      Detects BPM and displays track information from user-added songs using a simple peak-detection algorithm
-    `,
-    new Map([
-      ["Frontend", ["JavaScript", "Web Audio API", "React.js", "Redux.js"]],
-    ]),
-    "https://github.com/mrlucciola/bpm-detection-algorithm.git",
-    "/images/bda-preview.png",
-    "https://mrlucciola.github.io/bpm-detection-algorithm/"
-  ),
-  new Project(
-    "Cofi",
-    `\
-      A digital interface for synthesizing musical sequences in a step-wise manner.\
-      Simply point and click on the note you want to activate, and the time at which to activate.\
-    `,
-    new Map([["Frontend", ["React.js", "Web Audio API", "Redux.js"]]]),
-    "https://github.com/mrlucciola/cofi.git",
-    "/images/cofi-preview.png",
-    "https://mrlucciola.github.io/cofi/"
-  ),
+  // new Project(
+  //   "Portfolio Page",
+  //   `\
+  //     Rocco Lucciola's portfolio page
+  //   `,
+  //   new Map([["Frontend", ["React.js", "MobX", "Typescript"]]]),
+  //   "https://github.com/mrlucciola/dev-site/",
+  //   undefined,
+  //   "https://mrlucciola.com/"
+  // ),
+  // new Project(
+  //   "PitchClean",
+  //   `\
+  //     Social media investment platform focused on renewables (mock backend)
+  //   `,
+  //   new Map([
+  //     ["Frontend", ["React.js", "D3.js", "Redux.js"]],
+  //     ["Backend", ["MongoDB", "Node.js", "Express.js"]],
+  //   ]),
+  //   "https://github.com/pitch-clean/client-ui",
+  //   "/images/envest-preview.png"
+  //   // envest.pro
+  // ),
+  // new Project(
+  //   "Diffuse",
+  //   `\
+  //     Collaborative songwriting software.\
+  //     Register, sign in, create and save projects to your profile.\
+  //     Add others to contribute to your songs and track the history of the songs progression!\
+  //   `,
+  //   new Map([
+  //     ["Frontend", ["React.js", "D3.js", "Redux.js"]],
+  //     ["Backend", ["MongoDB", "Node.js", "Django"]],
+  //   ]),
+  //   "https://github.com/mrlucciola/diffuse.git",
+  //   "/images/diffuse-preview.png"
+  //   // https://diffuse.dev
+  // ),
+  // new Project(
+  //   "BDA",
+  //   `
+  //     Detects BPM and displays track information from user-added songs using a simple peak-detection algorithm
+  //   `,
+  //   new Map([
+  //     ["Frontend", ["JavaScript", "Web Audio API", "React.js", "Redux.js"]],
+  //   ]),
+  //   "https://github.com/mrlucciola/bpm-detection-algorithm.git",
+  //   "/images/bda-preview.png",
+  //   "https://mrlucciola.github.io/bpm-detection-algorithm/"
+  // ),
+  // new Project(
+  //   "Cofi",
+  //   `\
+  //     A digital interface for synthesizing musical sequences in a step-wise manner.\
+  //     Simply point and click on the note you want to activate, and the time at which to activate.\
+  //   `,
+  //   new Map([["Frontend", ["React.js", "Web Audio API", "Redux.js"]]]),
+  //   "https://github.com/mrlucciola/cofi.git",
+  //   "/images/cofi-preview.png",
+  //   "https://mrlucciola.github.io/cofi/"
+  // ),
 ];
