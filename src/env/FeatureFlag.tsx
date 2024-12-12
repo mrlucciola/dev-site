133a1366071d6d99fb1946a97866b244ef327ede
Rocco Lucciola <47836741+mrlucciola@users.noreply.github.com>
2024-12-11 22:42:36 -0500

    52 add GitHub action to deploy to qa subdomain (#60)
    * feat: add app-env logic and feature flag component
    
    * feat: add bunfig.toml file
    
    * feat: add qa subdomain to CNAME file
    
    * feat: add prod build action

---
+++ src/env/FeatureFlag.tsx
@@ -0,0 +1,60 @@
+import { FC, PropsWithChildren } from "react";
+// utils
+import { currentEnv, envConfigLookup } from ".";
+import { RuntimeEnvKey, RuntimeEnvLevel } from "./interfaces";
+
+/** ### Flag a feature for a specific environment by wrapping with this component
+ * @todo add provider for environment
+ *
+ * Usage:
+ * ```tsx
+ * // Prevent entire set of routes from rendering
+ * <FeatureFlag maxAllowedEnv="qa" conditions={[isDcsEmail]}>
+ *   <Routes>
+ *     <Route path="user-management/*">
+ *       <Route index element={<UserAuthTable />} />
+ *       <Route path="user/:userRecId" element={<UserAuthDetail />} />
+ *       // ...
+ *     </Route>
+ *   </Routes>
+ * </FeatureFlag>
+ * ```
+ *
+ * @todo add environment variables to global state context
+ */
+const FeatureFlag: FC<
+  PropsWithChildren & {
+    maxAllowedEnv: RuntimeEnvKey | RuntimeEnvLevel;
+    /** If ***all*** conditions are true (excluding `maxAllowedEnv`): render component.
+     *
+     * Accepts a single boolean or an array of booleans.
+     */
+    conditions?: boolean | boolean[];
+  }
+> = ({ maxAllowedEnv, children, conditions = [] }) => {
+  const areCustomConditionsMet =
+    typeof conditions === "boolean" ? conditions : conditions.every((c) => c);
+
+  const maxAllowedEnvKey =
+    typeof maxAllowedEnv === "string"
+      ? RuntimeEnvKey.safeParse(maxAllowedEnv).data
+      : RuntimeEnvKey.options[RuntimeEnvLevel.safeParse(maxAllowedEnv).data ?? -1];
+  if (!maxAllowedEnvKey) {
+    console.error(`Invalid 'env' value: ${maxAllowedEnv}`);
+    return <></>;
+  }
+  const maxAllowedEnvConfig = envConfigLookup[maxAllowedEnvKey];
+
+  maxAllowedEnvKey &&
+    // @ts-ignore
+    console.log("Name of component wrapped in feature flag: ", children?.valueOf().type.name);
+
+  const shouldRender = currentEnv.level <= maxAllowedEnvConfig.level && areCustomConditionsMet;
+  console.warn("current:", currentEnv, "max", maxAllowedEnvConfig, { areCustomConditionsMet });
+
+  if (shouldRender) return <>{children}</>;
+
+  return <></>;
+};
+
+export default FeatureFlag;
