7f27f69649e551300613b31f4c01f6a9c0d46df4
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2024-12-13 18:24:13 -0500

    fix: value access for AppProvider

--- src/components/App/AppProvider.tsx
+++ src/components/App/AppProvider.tsx
@@ -1,22 +1,28 @@
 import { useState } from "react";
+// utils
 import useCtxFactory, { type ICtxState } from "../../util/useCtxFactory";
+// interfaces
 import { type Project } from "../../mobx/interfaces/project";
-import { projectsArr } from "../../projectConfigs";
+// data
+import { ActiveProjectKey, projectsLookup } from "../../projectConfigs";
 
 export const { useAppCtx, AppProvider } = useCtxFactory(() => {
-  const [projects, setProjects] = useState<Project[]>(projectsArr);
+  /** @deprecated remove - use `activeProjectKey`/`setActiveProjectKey` instead */
   const [activeProjectIdx, setActiveProjectIdx] = useState(0);
+  const [activeProjectKey, setActiveProjectKey] = useState<ActiveProjectKey>(
+    ActiveProjectKey.options[0]
+  );
 
   // Getters
-  const activeProject: Project = projects[activeProjectIdx];
+  const activeProject: Project = projectsLookup[activeProjectKey];
 
   return {
-    projects,
-    // @todo remove if unused
-    setProjects,
-
+    /** @deprecated remove - use `activeProjectKey` instead */
     activeProjectIdx,
+    /** @deprecated remove - use `setActiveProjectKey` instead */
     setActiveProjectIdx,
+    activeProjectKey,
+    setActiveProjectKey,
 
     // Getters
     activeProject,
