c02486b67a9e09463303f73c551bb93e30854a3e
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2024-12-18 15:47:03 -0500

    fix: nav logo layout

--- src/components/App/Nav/HomeButton.tsx
+++ src/components/App/Nav/HomeButton.tsx
@@ -1,17 +1,35 @@
 import { FC } from "react";
 // mui
-import Grid from "@mui/material/Unstable_Grid2";
+import Grid, { Grid2Props } from "@mui/material/Unstable_Grid2";
 import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import AvatarGroup from "@mui/material/AvatarGroup";
+import Avatar from "@mui/material/Avatar";
+import { blueGrey } from "@mui/material/colors";
+
+const TitleText: FC<Grid2Props> = ({ children, sx, ...gridProps }) => (
+  <Grid
+    component={Typography}
+    sx={{ fontWeight: 800, lineHeight: 1.25, color: "white", fontSize: "inherit", ...sx }}
+    {...gridProps}
+  >
+    {children}
+  </Grid>
+);
 
 const HomeButton: FC = () => (
-  <Grid container flexDirection="column" fontSize={25}>
-    <Grid container flexDirection="row" spacing={0.2}>
-      <Grid component={Typography} fontWeight={800} color="rgb(200,200,200)" fontSize="inherit">
-        Rocco
-      </Grid>
-    </Grid>
-    <Grid component={Typography} fontWeight={800} color="white" fontSize="inherit">
-      Lucciola
+  <Grid container direction="row" gap={1} sx={{ alignItems: "center" }}>
+    <AvatarGroup spacing={10}>
+      <Avatar sx={{ bgcolor: blueGrey[700], color: "rgba(18,18,18,.5)", zIndex: 0 }}>M</Avatar>
+      <Avatar sx={{ bgcolor: blueGrey[600], zIndex: 1 }}>R</Avatar>
+      <Avatar sx={{ bgcolor: blueGrey[400], zIndex: 2 }}>L</Avatar>
+    </AvatarGroup>
+
+    <Divider orientation="vertical" flexItem variant="middle" />
+
+    <Grid container direction="column" fontSize={25}>
+      <TitleText sx={{ color: "rgb(200,200,200)" }}>Rocco</TitleText>
+      <TitleText>Lucciola</TitleText>
     </Grid>
   </Grid>
 );
